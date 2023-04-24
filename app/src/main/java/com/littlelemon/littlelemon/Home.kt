46aac2d63e25915d00ca68a64eca9808736ef616
Teki <68933219+secondtime-github@users.noreply.github.com>
2023-04-24 18:10:05 +0900

    Set up filters

--- app/src/main/java/com/littlelemon/littlelemon/Home.kt
+++ app/src/main/java/com/littlelemon/littlelemon/Home.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -12,18 +13,28 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -33,14 +44,40 @@ import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
 import com.bumptech.glide.integration.compose.GlideImage
 import com.littlelemon.littlelemon.ui.theme.LittleLemonColor
 
+enum class Category(val categoryName: String) {
+    Starter("starters"),
+    Main("mains"),
+    Dessert("desserts"),
+    Drink("drinks")
+}
+
 @Composable
 fun Home(navController: NavHostController, databaseMenuItems: List<MenuItemRoom>) {
+    var searchPhrase by remember { mutableStateOf("") }
+    val menuItems = if (searchPhrase.isNotBlank()) {
+            databaseMenuItems
+                .filter { it.title.contains(searchPhrase, ignoreCase = true) }
+        } else {
+            databaseMenuItems
+        }
+
+    val groups = menuItems.groupBy { it.category }
+    var selectedCategory: Category? by remember { mutableStateOf(null) }
+    val selectedMenuItems = when (selectedCategory) {
+        Category.Starter -> groups["starters"] ?: emptyList()
+        Category.Dessert -> groups["desserts"] ?: emptyList()
+        Category.Main -> groups["mains"] ?: emptyList()
+        Category.Drink -> groups["drinks"] ?: emptyList()
+        else -> menuItems
+    }
+
     Column(
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Header(navController = navController)
-        Hero()
-        MenuItems(items = databaseMenuItems)
+        HeaderBar(navController = navController)
+        Hero(searchPhrase = searchPhrase, onValueChanged = { searchPhrase = it })
+        MenuBreakdown { selectedCategory = if (selectedCategory == it) null else it }
+        MenuItems(items = selectedMenuItems)
     }
 }
 
@@ -70,7 +107,7 @@ fun HomePreview() {
 }
 
 @Composable
-fun Header(navController: NavHostController) {
+fun HeaderBar(navController: NavHostController) {
     Row(
         horizontalArrangement = Arrangement.SpaceBetween,
         verticalAlignment = Alignment.CenterVertically,
@@ -98,7 +135,7 @@ fun Header(navController: NavHostController) {
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Hero() {
+fun Hero(searchPhrase: String, onValueChanged: (String) -> Unit) {
     Column(
         modifier = Modifier
             .fillMaxWidth()
@@ -143,9 +180,10 @@ fun Hero() {
         }
 
         TextField(
-            value = "",
-            onValueChange = {},
+            value = searchPhrase,
+            onValueChange = { onValueChanged(it) },
             placeholder = { Text(text = "Enter search phrase") },
+            leadingIcon = { Icon(Icons.Default.Search, contentDescription = "") },
             modifier = Modifier
                 .padding(top = 10.dp)
                 .fillMaxWidth()
@@ -209,4 +247,48 @@ fun MenuItem(item: MenuItemRoom) {
         thickness = 1.dp,
         color = LittleLemonColor.yellow
     )
+}
+
+@Composable
+fun MenuBreakdown(onClick: (category: Category) -> Unit) {
+    Text(
+        text = "ORDER FOR DELIVERY!",
+        fontSize = 18.sp,
+        color = LittleLemonColor.charcoal,
+        fontWeight = FontWeight.Bold,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(24.dp),
+    )
+    Row(
+        modifier = Modifier.padding(bottom = 10.dp)
+    ) {
+        Category.values().forEach {
+            CategoryButton(category = it) {
+                onClick(it)
+            }
+        }
+    }
+    Divider(
+        modifier = Modifier.padding(start = 24.dp, end = 24.dp),
+        thickness = 2.dp,
+        color = LittleLemonColor.yellow
+    )
+}
+
+@Composable
+fun CategoryButton(category: Category, onClick: () -> Unit) {
+    Button(
+        onClick = onClick,
+        colors = ButtonDefaults.buttonColors(LittleLemonColor.green.copy(alpha = 0.3f)),
+        contentPadding = PaddingValues(horizontal = 16.dp, vertical = 4.dp),
+        modifier = Modifier.padding(end = 8.dp)
+    ) {
+        Text(
+            text = category.categoryName,
+            fontSize = 14.sp,
+            color = LittleLemonColor.green,
+            textAlign = TextAlign.Center,
+        )
+    }
 }
